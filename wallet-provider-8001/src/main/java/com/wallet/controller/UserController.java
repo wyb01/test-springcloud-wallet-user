d3b4bdf7b985d74083eeb476e66e11723a26f151
wyb01 <zhangsitun@sina.com>
2019-09-27 19:27:43 +0800

    添加swagger功能

--- wallet-provider-8001/src/main/java/com/wallet/controller/UserController.java
+++ wallet-provider-8001/src/main/java/com/wallet/controller/UserController.java
@@ -2,6 +2,10 @@ package com.wallet.controller;
 
 import com.wallet.api.entity.User;
 import com.wallet.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -24,18 +28,30 @@ public class UserController {
     @Autowired
     private DiscoveryClient client;  //服务发现
 
+    @ApiOperation(value = "添加用户",notes = "添加用户")
+    @ApiImplicitParams({
+    	@ApiImplicitParam(name = "user",value = "用户详情实体类")
+    })
     @RequestMapping(value = "/user/add", method = RequestMethod.POST)
     public boolean add(@RequestBody User user)
     {
         return service.add(user);
     }
 
+    @ApiOperation(value = "获取用户",notes = "根据用户id获取用户信息")
+    @ApiImplicitParams({
+   	@ApiImplicitParam(name = "id",value = "")
+    })
     @RequestMapping(value = "/user/get/{id}", method = RequestMethod.GET)
     public User get(@PathVariable("id") Long id)
     {
         return service.get(id);
     }
 
+    @ApiOperation(value = "用户全查询",notes = "查询全部用户")
+    @ApiImplicitParams({
+    	@ApiImplicitParam(name = "null",value = "")
+    })
     @RequestMapping(value = "/user/list", method = RequestMethod.GET)
     public List<User> list()
     {
